cdf32a71247eb9dfab3d6711c27cf915f6d6de32
Daniel1996e <43450731+Daniel1996e@users.noreply.github.com>
2020-10-16 23:00:09 -0500

    taller-8-Daniel1996e

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -21,7 +21,7 @@ public class Jugador extends Futbolista{
 	}
 	public String toString() {
 		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() 
-		+ "con el dorsal" + this.dorsal + ". Ha marcado " + this.golesMarcados;
+		+ " con el dorsal " + this.dorsal + ". Ha marcado " + this.golesMarcados;
 	}
 	public boolean jugarConLasManos() {
 		return false;
--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -16,7 +16,7 @@ public class Portero extends Futbolista {
 	}
 	public String toString() {
 		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() 
-		+ "con el dorsal" + this.dorsal + ". Le han marcado " + this.golesRecibidos;
+		+ " con el dorsal " + this.dorsal + ". Le han marcado " + this.golesRecibidos;
 	}
 	public boolean jugarConLasManos() {
 		return true;
