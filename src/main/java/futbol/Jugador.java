76a077a812dd0cd301111d01495d110b7d41f421
Daniel1996e <43450731+Daniel1996e@users.noreply.github.com>
2020-10-16 22:02:48 -0500

    taller-8-Daniel1996e

---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,29 @@
+package futbol;
+
+public class Jugador extends Futbolista{
+	public short golesMarcados;
+	public byte dorsal;
+	
+	public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal) {
+		super(nombre, edad, posicion);
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+	}
+	public Jugador() {
+		super();
+		this.golesMarcados = 289;
+		this.dorsal = 7;
+	}
+	
+	@Override
+	public int compareTo(Futbolista o) {
+		return Math.abs(this.getEdad()-o.getEdad());
+	}
+	public String toString() {
+		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() 
+		+ "con el dorsal" + this.dorsal + ". Ha marcado " + this.golesMarcados;
+	}
+	public boolean jugarConLasManos() {
+		return false;
+	}
+}
---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,24 @@
+package futbol;
+
+public class Portero extends Futbolista {
+	public short golesRecibidos;
+	public byte dorsal;
+	
+	public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+		super(nombre, edad, "Portero");
+		this.golesRecibidos = golesRecibidos;
+		this.dorsal = dorsal;
+	}
+	
+	@Override
+	public int compareTo(Portero o) {
+		return Math.abs(this.golesRecibidos -o.golesRecibidos);
+	}
+	public String toString() {
+		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() 
+		+ "con el dorsal" + this.dorsal + ". Le han marcado " + this.golesRecibidos;
+	}
+	public boolean jugarConLasManos() {
+		return true;
+	}
+}
